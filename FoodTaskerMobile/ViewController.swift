06f2ee51ede01e7847c1e4bee9e1eb4ee3c5f21c
buriedguns <blivion21@yandex.ru>
2018-12-06 12:44:09 +0300

    Initial Commit

---
+++ FoodTaskerMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Макс on 06/12/2018.
+//  Copyright © 2018 Макс. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
