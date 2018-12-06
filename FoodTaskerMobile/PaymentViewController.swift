e12ab3d8c0c84d869e8e25942352f46d1baf63b1
buriedguns <blivion21@yandex.ru>
2018-12-06 20:09:01 +0300

    67_17

---
+++ FoodTaskerMobile/PaymentViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PaymentViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Макс on 06/12/2018.
+//  Copyright © 2018 Макс. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class PaymentViewController: UIViewController {
+
+    @IBOutlet weak var cardTextField: STPPaymentCardTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
--- FoodTaskerMobile/TrayViewController.swift
+++ FoodTaskerMobile/TrayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RestaurantViewController.swift
+//  TrayViewController.swift
 //  FoodTaskerMobile
 //
 //  Created by Макс on 06/12/2018.
@@ -8,29 +8,33 @@
 
 import UIKit
 
-class RestaurantViewController: UIViewController {
+class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if self.revealViewController() != nil {
             menuBarButton.target = self.revealViewController()
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+
     }
+    
+
 }
-extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
+
+extension TrayViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trayItemCell", for: indexPath)
         return cell
     }
 }
